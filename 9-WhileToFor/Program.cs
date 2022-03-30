71b43d319905f0aa697c6f8ae711a6f522f5e81a
Lorena Borges <lobcborges@gmail.com>
2022-03-30 12:03:32 -0300

    finalizacao curso

---
+++ 9-WhileToFor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_WhileToFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int contador = 1; contador <= 10; contador++)
+            {
+                Console.WriteLine(contador);
+            }
+            Console.ReadLine();
+        }
+    }
+}
